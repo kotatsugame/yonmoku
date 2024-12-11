66a1c9354792b60c2f20ff88bf2c0b8ac382a10d
kotatsugame <takaryou1019@gmail.com>
2024-12-12 05:43:57 +0900

    update

--- yonmoku.cpp
+++ yonmoku.cpp
@@ -11,12 +11,14 @@
 using namespace std;
 
 mt19937 rng(random_device{}());
+//mt19937 rng;
 enum Cell{None, Me, You};
 enum State{Continue, End, Invalid};
 enum Color{Draw, Black, White};
 
 const int SIZE = 4;
 #define IDX(x, y, z) ((x) + (y) * SIZE + (z) * SIZE * SIZE)
+#define BIT(x, y, z) (1uLL << IDX(x, y, z))
 #define X(idx) ((idx) % SIZE)
 #define Y(idx) ((idx) / SIZE % SIZE)
 #define Z(idx) ((idx) / SIZE / SIZE)
@@ -28,18 +30,88 @@ array<unsigned long long, LINES_NUM> LINES;
 
 struct Board
 {
-	unsigned long long Me,You;
-	Board():Me(0uLL),You(0uLL){}
+	unsigned long long Me, You;
+	Board() : Me(0uLL), You(0uLL) {}
 
-	inline enum Cell get_cell(int x,int y,int z) const
+	static inline enum State win(unsigned long long B)
 	{
-		int idx = IDX(x, y, z);
-		if(Me >> idx & 1uLL)
-		{
-			assert(!(You >> idx & 1uLL));
-			return Cell::Me;
+		{//z dir
+			unsigned long long b = B & B >> 2 * SIZE * SIZE;
+			//static const unsigned long long mask = 0x000000000000ffffuLL;
+			if (b & b >> SIZE * SIZE) return State::End;
 		}
-		else if(You >> idx & 1uLL) return Cell::You;
+		{//x dir
+			unsigned long long b = B & B >> 2;
+			static const unsigned long long mask = 0x1111111111111111uLL;
+			if (b & b >> 1 & mask) return State::End;
+		}
+		{//y dir
+			unsigned long long b = B & B >> 2 * SIZE;
+			static const unsigned long long mask = 0x000f000f000f000fuLL;
+			if (b & b >> SIZE & mask) return State::End;
+		}
+		{//yz dir
+			{
+				unsigned long long b = B & B >> 2 * (SIZE * SIZE + SIZE);
+				//static const unsigned long long mask = 0x000000000000000fuLL;
+				if (b & b >> SIZE * SIZE + SIZE) return State::End;
+			}
+			{
+				unsigned long long b = B & B >> 2 * (SIZE * SIZE - SIZE);
+				static const unsigned long long mask = 0x000000000000f000uLL;
+				if (b & b >> SIZE * SIZE - SIZE & mask) return State::End;
+			}
+		}
+		{//zx dir
+			{
+				unsigned long long b = B & B >> 2 * (SIZE * SIZE + 1);
+				static const unsigned long long mask = 0x0000000000001111uLL;
+				if (b & b >> SIZE * SIZE + 1 & mask) return State::End;
+			}
+			{
+				unsigned long long b = B & B >> 2 * (SIZE * SIZE - 1);
+				static const unsigned long long mask = 0x0000000000008888uLL;
+				if (b & b >> SIZE * SIZE - 1 & mask) return State::End;
+			}
+		}
+		{//xy dir
+			{
+				unsigned long long b = B & B >> 2 * (SIZE + 1);
+				static const unsigned long long mask = 0x0001000100010001uLL;
+				if (b & b >> SIZE + 1 & mask) return State::End;
+			}
+			{
+				unsigned long long b = B & B >> 2 * (SIZE - 1);
+				static const unsigned long long mask = 0x0008000800080008uLL;
+				if (b & b >> SIZE - 1 & mask) return State::End;
+			}
+		}
+		{//xyz dir
+			{
+				static const unsigned long long line = BIT(0, 0, 0) | BIT(1, 1, 1) | BIT(2, 2, 2) | BIT(3, 3, 3);
+				if ((B & line) == line) return State::End;
+			}
+			{
+				static const unsigned long long line = BIT(0, 0, 3) | BIT(1, 1, 2) | BIT(2, 2, 1) | BIT(3, 3, 0);
+				if ((B & line) == line) return State::End;
+			}
+			{
+				static const unsigned long long line = BIT(0, 3, 0) | BIT(1, 2, 1) | BIT(2, 1, 2) | BIT(3, 0, 3);
+				if ((B & line) == line) return State::End;
+			}
+			{
+				static const unsigned long long line = BIT(0, 3, 3) | BIT(1, 2, 2) | BIT(2, 1, 1) | BIT(3, 0, 0);
+				if ((B & line) == line) return State::End;
+			}
+		}
+		return State::Continue;
+	}
+
+	inline enum Cell get_cell(int x, int y, int z) const
+	{
+		unsigned long long bit = BIT(x, y, z);
+		if (Me & bit)return Cell::Me;
+		else if (You & bit) return Cell::You;
 		else return Cell::None;
 	}
 
@@ -70,9 +142,9 @@ struct Board
 			{
 				for (int x = 0; x < SIZE; x++)
 				{
-					const int idx = IDX(x, y, z);
-					if (black >> idx & 1uLL) cout << "\033[31mX\033[m";
-					else if (white >> idx & 1uLL) cout << "O";
+					const unsigned long long bit = BIT(x, y, z);
+					if (black & bit) cout << "\033[31mX\033[m";
+					else if (white & bit) cout << "O";
 					else cout << "-";
 				}
 				if (z + 1 < SIZE) cout << " ";
@@ -87,50 +159,42 @@ struct Board
 		array<int, LINES_NUM> ret;
 		for (int i = 0; i < LINES_NUM; i++)
 		{
-			if ((Me & LINES[i]) and (You & LINES[i])) ret[i] = 0;
+			if ((Me & LINES[i]) && (You & LINES[i])) ret[i] = 0;
 			else if (Me & LINES[i]) ret[i] = __builtin_popcountll(Me & LINES[i]);
-			else ret[i] = -__builtin_popcountll(You & LINES[i]);
+			else ret[i] = - __builtin_popcountll(You & LINES[i]);
 		}
 		return ret;
 	}
 
-	enum State win() const
-	{//can be more speed up
-		for (const unsigned long long L: LINES)
-		{
-			if ((You & L) == L)return State::End;
-		}
-		return State::Continue;
-	}
-
 	enum State place(int x, int y)
 	{
-		if(x<0||4<=x||y<0||4<=y)
+		if (x < 0 || 4 <= x || y < 0 || 4 <= y)
 		{
-			cout<<"out of range : ("<<x+1<<", "<<y+1<<")"<<endl;
+			cout << "out of range : (" << x + 1 << ", " << y + 1 << ")" << endl;
 			return State::Invalid;
 		}
-		int z=0;
-		while(z<4 && get_cell(x, y, z) != Cell::None) z++;
-		if(z==4)
+		int z = 0;
+		while (z < 4 && get_cell(x, y, z) != Cell::None) z++;
+		if (z == 4)
 		{
-			cout<<"row ("<<x+1<<", "<<y+1<<") is full"<<endl;
+			cout << "row (" << x + 1 << ", " << y + 1 << ") is full" << endl;
 			return State::Invalid;
 		}
-		Me |= 1uLL << IDX(x, y, z);
+		Me |= BIT(x, y, z);
 		swap(Me, You);
-		return win();
+		return win(You);
 	}
 
-	enum State place_fast(unsigned long long xyz)
+	enum State place_fast(int xyz)
 	{
 		Me |= 1uLL << xyz;
 		swap(Me, You);
-		return win();
+		return win(You);
 	}
 
 	unsigned long long valid_move() const
 	{
+		/*
 		unsigned long long ret = 0uLL;
 		for(int x=0;x<SIZE;x++)for(int y=0;y<SIZE;y++)
 		{
@@ -138,21 +202,25 @@ struct Board
 			while(z<4 && get_cell(x, y, z) != Cell::None) z++;
 			if(z<4) ret |= 1uLL << IDX(x, y, z);
 		}
-		return ret;
+		assert(ret == (((Me | You) << SIZE * SIZE | ((1uLL << SIZE * SIZE) - 1)) & ~(Me | You)));
+		*/
+		return ((Me | You) << SIZE * SIZE | ((1uLL << SIZE * SIZE) - 1)) & ~(Me | You);
 	}
 };
 
 struct Player
 {
+	bool verbose;
+	void set_verbose(bool verbose_) {verbose = verbose_;}
 	virtual pair<int,int> move(Board board) = 0;
 };
 
 struct HumanPlayer : Player
 {
-	pair<int,int> move(Board board) override
+	pair<int, int> move(Board board) override
 	{
 		enum Color now = board.validate();
-		cout<<endl;
+		cout << endl;
 		board.print();
 		cout << "Input x y: place ";
 		if (now == Color::Black) cout << "\033[31mX\033[m";
@@ -169,42 +237,39 @@ struct Game
     Board board;
     Player* player1;
     Player* player2;
-	vector<pair<int,int> >hand;
-	bool verbose, start;
+	vector<pair<int, int> > hand, start;
+	bool verbose;
 
-    Game(Player* p1, Player* p2, bool verbose=false, bool start=false) : player1(p1), player2(p2), verbose(verbose), start(start) {}
+    Game(Player* p1, Player* p2, bool verbose=false, vector<pair<int, int> > start = {}) : player1(p1), player2(p2), verbose(verbose), start(start)
+	{
+		p1 -> set_verbose(verbose);
+		p2 -> set_verbose(verbose);
+	}
 
 	enum State move(int turn)
 	{
 		enum State ret;
-		while(true)
+		while (true)
 		{
-			auto st=chrono::system_clock::now();
-			int x,y;
-			if(start&&turn<4)
-			{
-				static const int SX[4]={0,3,0,3};
-				static const int SY[4]={0,3,3,0};
-				x=SX[turn];
-				y=SY[turn];
-			}
+			auto st = chrono::system_clock::now();
+			pair<int, int> xy;
+			if (turn < start.size()) xy = start[turn];
 			else
 			{
 				Player* current_player = (turn % 2 == 0) ? player1 : player2;
-				auto p = current_player->move(board);
-				x=p.first; y=p.second;
+				xy = current_player -> move(board);
 			}
-			ret = board.place(x,y);
+			ret = board.place(xy.first, xy.second);
 			if (ret == State::Invalid)
 			{
-				cout<<"Invalid move : ("<<x+1<<", "<<y+1<<")"<<endl;
+				cout << "Invalid move : (" << xy.first + 1 << ", " << xy.second + 1 << ")" << endl;
 				continue;
 			}
-			hand.emplace_back(x+1, y+1);
-			if(verbose)
+			hand.push_back(xy);
+			if (verbose)
 			{
-				auto msec=chrono::duration_cast<std::chrono::milliseconds>(chrono::system_clock::now() - st);
-				cout << "[turn " << turn + 1 << "] Place to (" << x + 1 << ", " << y + 1 << ") ";
+				auto msec = chrono::duration_cast<std::chrono::milliseconds>(chrono::system_clock::now() - st);
+				cout << "[turn " << turn + 1 << "] Place to (" << xy.first + 1 << ", " << xy.second + 1 << ") ";
 				if (turn % 2 == 0) cout << "\033[31m(Black)\033[m";
 				else cout << "(White)";
 				cout << " by " << msec.count() / 1e3 << " sec" << endl;
@@ -218,14 +283,14 @@ struct Game
 	enum Color game()
 	{
 		enum State ret;
-		for(int turn=0;turn<BOARD_SIZE;turn++)
+		for (int turn = 0; turn < BOARD_SIZE; turn++)
 		{
-			ret=move(turn);
-			if(ret == State::End)break;
+			ret = move(turn);
+			if (ret == State::End) break;
 		}
 		board.print();
 		enum Color result;
-		if(ret == State::End)
+		if (ret == State::End)
 		{
 			cout << "END : winner is ";
 			if (board.validate() == Color::White)
@@ -245,27 +310,27 @@ struct Game
 			result = Color::Draw;
 			cout << "DRAW" << endl;
 		}
-		cout<<"hands :";
-		for(auto[x,y]:hand)cout<<" ("<<x<<", "<<y<<"),";
-		cout<<endl;
+		cout << "hands :";
+		for (auto [x,y] : hand) cout << " (" << x << ", " << y << "),";
+		cout << endl;
 		return result;
 	}
 };
 
 
 template<typename F>
-pair<unsigned long long,double>read_DFS(Board board,int level,F evaluate_func)
+pair<unsigned long long, double> read_DFS (Board board, int level, const F& evaluate_func)
 {
 	pair<unsigned long long, double> mv;
 	mv.first = 0uLL;
 
-	if(level==0)
+	if (level == 0)
 	{
-		mv.second=evaluate_func(board);
+		mv.second = evaluate_func(board);
 		return mv;
 	}
 
-	assert(level>=1);
+	assert(level >= 1);
 	unsigned long long hand = board.valid_move();
 	if (!hand)
 	{
@@ -273,51 +338,51 @@ pair<unsigned long long,double>read_DFS(Board board,int level,F evaluate_func)
 		return mv;
 	}
 
-	mv.second=-INF;
-	for (int xyz = 0; xyz < BOARD_SIZE; xyz++) if (hand >> xyz & 1uLL)
+	mv.second = -INF;
+	for (int xyz = 0; xyz < BOARD_SIZE; xyz++) if (hand & 1uLL << xyz)
 	{
-		Board b=board;
-		enum State r=b.place_fast(xyz);
+		Board b = board;
+		enum State r = b.place_fast(xyz);
 		assert(r != State::Invalid);
 		double ev;
 		if (r == State::End) ev = INF;
 		else
 		{
-			ev = -read_DFS(b, level-1, evaluate_func).second;
-			if (ev > INF/10) ev = ev - 1;
-			else if (ev < -INF/10) ev = ev + 1;
+			ev = -read_DFS(b, level - 1, evaluate_func).second;
+			if (ev > INF - BOARD_SIZE * 2) ev = ev - 1;
+			else if (ev < -INF + BOARD_SIZE * 2) ev = ev + 1;
 		}
-		if(mv.second<ev)
+		if (mv.second < ev)
 		{
 			mv.first = 0uLL;
 			mv.second = ev;
 		}
-		if(mv.second==ev) mv.first |= 1uLL << xyz;
+		if (mv.second == ev) mv.first |= 1uLL << xyz;
 	}
 	assert(mv.first);
 	return mv;
 }
 
 template<typename F>
-struct AIPlayer:Player
+struct AIPlayer : Player
 {
+	using Player::verbose;
 	int level;
 	F evaluate_func;
-	AIPlayer(int level,F evaluate_func):level(level),evaluate_func(evaluate_func){}
+	AIPlayer(int level, F evaluate_func) : level(level), evaluate_func(evaluate_func) {}
 
-    pair<int,int> move(Board board) override
+    pair<int, int> move(Board board) override
 	{
 		unsigned long long hand = board.valid_move();
 		assert(hand);
 
-		auto ret=board.count();
-		for(const int v:ret)assert(abs(v)<4);
+		auto ret = board.count();
+		for(const int v : ret) assert(abs(v) < 4);
 		for(int i=0;i<ret.size();i++)if(ret[i]==3)
 		{
 			if(hand & LINES[i])
 			{
-				int v = 0;
-				while (!((hand & LINES[i]) >> v & 1)) v++;
+				int v = __builtin_ctzll(hand & LINES[i]);
 				return make_pair(X(v), Y(v));
 			}
 		}
@@ -325,116 +390,133 @@ struct AIPlayer:Player
 		{
 			if(hand & LINES[i])
 			{
-				int v = 0;
-				while (!((hand & LINES[i]) >> v & 1)) v++;
+				int v = __builtin_ctzll(hand & LINES[i]);
 				return make_pair(X(v), Y(v));
 			}
 		}
 
 		const int turn = board.turn();// turn number (the number of stones = turn - 1)
-		auto[mv, ev] = read_DFS(board, turn >= 40 ? 10 : level, evaluate_func);
+		auto[mv, ev] = read_DFS(board, turn >= 40 ? level : level, evaluate_func);
 		enum Color now = board.validate();
-		if (now == Color::Black) cout<<"\033[31mscore = "<<ev<<" by";
-		else cout<<"score = "<<ev<<" by";
-		for (int xyz = 0; xyz < BOARD_SIZE; xyz++) if (mv >> xyz & 1uLL)
+		if (verbose)
 		{
-			cout<<" ("<<X(xyz)+1<<", "<<Y(xyz)+1<<", "<<Z(xyz)+1<<"),";
+			if (now == Color::Black) cout<<"\033[31m";
+			cout << "score = " << ev << " by";
+			for (int xyz = 0; xyz < BOARD_SIZE; xyz++) if (mv & 1uLL << xyz)
+			{
+				cout << " (" << X(xyz) + 1 << ", " << Y(xyz) + 1 << ", " << Z(xyz) + 1 << "),";
+			}
+			if (now == Color::Black) cout<<"\033[m";
+			cout<<endl;
 		}
-		if (now == Color::Black) cout<<"\033[m";
-		cout<<endl;
 		assert(mv);
-		vector<pair<int,int> >XY;
-		for (int xyz = 0; xyz < BOARD_SIZE; xyz++) if (mv >> xyz & 1uLL) XY.emplace_back(X(xyz), Y(xyz));
-		return XY[rng()%XY.size()];
+		{
+			vector<pair<int, int> > XY;
+			for (int xyz = 0; xyz < BOARD_SIZE; xyz++) if (mv & 1uLL << xyz) XY.emplace_back(X(xyz), Y(xyz));
+			return XY[rng() % XY.size()];
+		}
 	}
 };
 
 int main()
 {
 	{
-		vector<unsigned long long>lines;
-		for (int x = 0; x < SIZE; ++x) {
-			for (int y = 0; y < SIZE; ++y) {
-				unsigned long long line = 0;
-				for (int z = 0; z < SIZE; ++z) {
-					line |= 1uLL << IDX(x, y, z);
-				}
-				lines.push_back(line);
-			}
-		}
-		for (int y = 0; y < SIZE; ++y) {
-			for (int z = 0; z < SIZE; ++z) {
-				unsigned long long line = 0;
-				for (int x = 0; x < SIZE; ++x) {
-					line |= 1uLL << IDX(x, y, z);
-				}
-				lines.push_back(line);
-			}
-		}
-		for (int z = 0; z < SIZE; ++z) {
-			for (int x = 0; x < SIZE; ++x) {
-				unsigned long long line = 0;
-				for (int y = 0; y < SIZE; ++y) {
-					line |= 1uLL << IDX(x, y, z);
-				}
-				lines.push_back(line);
-			}
-		}
-
-		for (int x = 0; x < SIZE; ++x) {
-			unsigned long long line1 = 0, line2 = 0;
-			for (int yz = 0; yz < SIZE; ++yz) {
-				line1 |= 1uLL << IDX(x, yz, yz);
-				line2 |= 1uLL << IDX(x, yz, SIZE - 1 - yz);
-			}
-			lines.push_back(line1);
-			lines.push_back(line2);
-		}
-		for (int y = 0; y < SIZE; ++y) {
-			unsigned long long line1 = 0, line2 = 0;
-			for (int zx = 0; zx < SIZE; ++zx) {
-				line1 |= 1uLL << IDX(zx, y, zx);
-				line2 |= 1uLL << IDX(zx, y, SIZE - 1 - zx);
-			}
-			lines.push_back(line1);
-			lines.push_back(line2);
-		}
-		for (int z = 0; z < SIZE; ++z) {
-			unsigned long long line1 = 0, line2 = 0;
-			for (int xy = 0; xy < SIZE; ++xy) {
-				line1 |= 1uLL << IDX(xy, xy, z);
-				line2 |= 1uLL << IDX(xy, SIZE - 1 - xy, z);
-			}
-			lines.push_back(line1);
-			lines.push_back(line2);
-		}
-
+		vector<unsigned long long> lines;
+		for (int x = 0; x < SIZE; x++) for (int y = 0; y < SIZE; y++)
 		{
-			unsigned long long line1 = 0, line2 = 0, line3 = 0, line4 = 0;
-			for (int xyz = 0; xyz < SIZE; ++xyz) {
-				line1 |= 1uLL << IDX(xyz, xyz, xyz);
-				line2 |= 1uLL << IDX(xyz, xyz, SIZE - 1 - xyz);
-				line3 |= 1uLL << IDX(xyz, SIZE - 1 - xyz, xyz);
-				line4 |= 1uLL << IDX(xyz, SIZE - 1 - xyz, SIZE - 1 - xyz);
+			unsigned long long line = 0uLL;
+			for (int z = 0; z < SIZE; z++) line |= BIT(x, y, z);
+			lines.push_back(line);
+		}
+		for (int y = 0; y < SIZE; y++) for (int z = 0; z < SIZE; z++)
+		{
+			unsigned long long line = 0uLL;
+			for (int x = 0; x < SIZE; x++) line |= BIT(x, y, z);
+			lines.push_back(line);
+		}
+		for (int z = 0; z < SIZE; z++) for (int x = 0; x < SIZE; x++)
+		{
+			unsigned long long line = 0uLL;
+			for (int y = 0; y < SIZE; y++) line |= BIT(x, y, z);
+			lines.push_back(line);
+		}
+		for (int x = 0; x < SIZE; x++)
+		{
+			unsigned long long line1 = 0uLL, line2 = 0uLL;
+			for (int yz = 0; yz < SIZE; yz++)
+			{
+				line1 |= BIT(x, yz, yz);
+				line2 |= BIT(x, yz, SIZE - 1 - yz);
+			}
+			lines.push_back(line1);
+			lines.push_back(line2);
+		}
+		for (int y = 0; y < SIZE; y++)
+		{
+			unsigned long long line1 = 0uLL, line2 = 0uLL;
+			for (int zx = 0; zx < SIZE; zx++)
+			{
+				line1 |= BIT(zx, y, zx);
+				line2 |= BIT(zx, y, SIZE - 1 - zx);
+			}
+			lines.push_back(line1);
+			lines.push_back(line2);
+		}
+		for (int z = 0; z < SIZE; z++)
+		{
+			unsigned long long line1 = 0uLL, line2 = 0uLL;
+			for (int xy = 0; xy < SIZE; xy++)
+			{
+				line1 |= BIT(xy, xy, z);
+				line2 |= BIT(xy, SIZE - 1 - xy, z);
+			}
+			lines.push_back(line1);
+			lines.push_back(line2);
+		}
+		{
+			unsigned long long line1 = 0uLL, line2 = 0uLL, line3 = 0uLL, line4 = 0uLL;
+			for (int xyz = 0; xyz < SIZE; xyz++)
+			{
+				line1 |= BIT(xyz, xyz, xyz);
+				line2 |= BIT(xyz, xyz, SIZE - 1 - xyz);
+				line3 |= BIT(xyz, SIZE - 1 - xyz, xyz);
+				line4 |= BIT(xyz, SIZE - 1 - xyz, SIZE - 1 - xyz);
 			}
 			lines.push_back(line1);
 			lines.push_back(line2);
 			lines.push_back(line3);
 			lines.push_back(line4);
 		}
-
-		assert(lines.size()==LINES_NUM);
-		for(int j=0;j<LINES_NUM;j++)
+		assert(lines.size() == LINES_NUM);
+		for (int j = 0; j < LINES_NUM; j++)
 		{
 			LINES[j] = lines[j];
 			assert(__builtin_popcountll(LINES[j]) == 4);
+			assert(Board::win(LINES[j]) == State::End);
 		}
+
+		auto dfs = [](auto self, int id, unsigned long long B, int rest) {
+			if (id == 64)
+			{
+				if (rest > 0) return;
+				bool win = false;
+				for (unsigned long long L : LINES) if ((L & B) == L) win = true;
+				assert(win == (Board::win(B) == State::End));
+			}
+			else
+			{
+				self(self, id + 1, B, rest);
+				if (rest > 0) self(self, id + 1, B | 1uLL << id, rest - 1);
+			}
+		};
+		//validation of checking win
+		dfs(dfs, 0, 0uLL, 4);
 	}
 
-	auto evaluate=[](const Board&board)->double
+	auto evaluate = [](const Board&board) -> double
 	{
-		int sum=0;
-		for(int v:board.count())sum+=v;
+		int sum = 0;
+		for(int v : board.count()) sum += v;
 		return (double)sum;
 	};
 	/*
@@ -555,23 +637,21 @@ int main()
 	//Game game(&H, &AI, true, true);
 	//game.game();
 	//return 0;
-	AIPlayer p1(6,evaluate);
-	AIPlayer p2(6,evaluate);
-	Game game(&p1, &p2, true, true);
+	AIPlayer p1(7, evaluate);
+	AIPlayer p2(7, evaluate);
+	Game game(&p1, &p2, true, {{0, 0}, {3, 3}, {0, 3}, {3, 0}});
 	game.game();
 	return 0;
-	/*
-	   int cnt[3]={};
-	   for(int t=1;;t++)
-	   {
-	   cout<<"Game #"<<t<<endl;
-	   Game game(&p1, &p2,false,true);
-	   int r=game.game();
-	   cnt[r+1]++;
-	   cout<<"Black : "<<cnt[2]<<endl;
-	   cout<<"White : "<<cnt[0]<<endl;
-	   cout<<" Draw : "<<cnt[1]<<endl;
-	   }
-	 */
+	int cnt[3] = {};
+	for(int t=1;;t++)
+	{
+		cout<<"Game #"<<t<<endl;
+		Game game(&p1, &p2, false, {{0, 0}, {3, 3}, {0, 3}, {3, 0}});
+		enum Color r = game.game();
+		cnt[r]++;
+		cout<<"Black : "<<cnt[Color::Black]<<endl;
+		cout<<"White : "<<cnt[Color::White]<<endl;
+		cout<<" Draw : "<<cnt[Color::Draw]<<endl;
+	}
 	return 0;
 }
